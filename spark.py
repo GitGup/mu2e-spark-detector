e0a0773f4e6c62e8f9196dd5e466ef149040a5ea
Gup <gups.singhs@gmail.com>
2024-07-19 14:13:10 -0700

    first commit, added main script and files
    main script, test footage, and required packages added

---
+++ spark.py
@@ -0,0 +1,102 @@
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import glob
+import re
+import argparse
+
+sns.set_theme(style="white")
+
+frame_count = 0
+
+def process_video(path):
+    global frame_count
+    cap = cv2.VideoCapture(path)
+
+    frame_dir = "frames"
+
+    if not os.path.exists(frame_dir):
+        os.makedirs(frame_dir)
+        
+    if not cap.isOpened():
+        print("Error")
+        exit()
+
+    while cap.isOpened():
+        #print("cap open")
+        
+        ret, frame = cap.read() 
+
+        if ret:
+            frame_path = frame_dir + f"/frame_{frame_count}.jpg"
+            cv2.imwrite(frame_path, frame)
+
+            frame_count += 1
+        else: 
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print("Done Processing!")
+
+def extract_frame_number(file_path):
+    match = re.search(r'frame_(\d+)', file_path)
+    return int(match.group(1)) if match else None
+
+#detect AMB spark given an image/frame
+def detect_spark(image, frame_count):
+
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    #adjsut mask if we miss sparks
+    lower_blue = np.array([85, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+    mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
+    
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    found_spark = False
+    for contour in contours:
+        if cv2.contourArea(contour) > 25:
+            
+            #means we found a spark so draw a red square
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+            #save the sparked AMB image/frame
+            output_dir = "AMB_Sparks"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            spark_image_path = os.path.join(output_dir, f"AMB_Spark_{frame_count}.png")
+            plt.imsave(spark_image_path, cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            
+            found_spark = True
+
+            print(f"Spark Detected on frame {frame_count}")
+
+    found_spark = False
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Process video and detect sparks in images.")
+    parser.add_argument('video_path', type=str, help='Path to the video file')
+
+    args = parser.parse_args()
+
+    raw_video_path = args.video_path
+    
+    print("Processing video from: ", raw_video_path)
+    process_video(raw_video_path)
+
+    image_files = glob.glob('frames/*.jpg')
+    sorted_image_files = sorted(image_files, key=extract_frame_number)
+
+    frame_count = 0
+    for file_path in sorted_image_files:
+        image = cv2.imread(file_path)
+        detect_spark(image, frame_count)
+        frame_count+=1
